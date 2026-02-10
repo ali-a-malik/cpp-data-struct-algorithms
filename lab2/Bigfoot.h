4f07aa44e0cef9b2025f7cba1397353215475856
Ali A Malik <ali.ahmed.malik.07@gmail.com>
2026-02-09 22:13:03 -0800

    A: initial commit

---
+++ lab2/Bigfoot.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+class Bigfoot {
+    private: 
+        double height;
+        double weight;
+        double shoeSize;
+        std::string hairColor;
+        
+
+};
