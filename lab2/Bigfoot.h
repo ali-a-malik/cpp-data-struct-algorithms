43dc4812467487141eb7c53d8286c74fd734d614
Ali A Malik <ali.ahmed.malik.07@gmail.com>
2026-02-09 22:34:40 -0800

    B: Bigfoot.h completed

--- lab2/Bigfoot.h
+++ lab2/Bigfoot.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
 #include <string>
+
 
 class Bigfoot {
     private: 
@@ -8,6 +11,20 @@ class Bigfoot {
         double weight;
         double shoeSize;
         std::string hairColor;
-        
+    public:
+        double getHeight() const {return height;};
+        double getWeight() const {return weight;};
+        double getShozeSize() const{return shoeSize;};
+        std::string getHairColor() const{return hairColor;};
 
+        void setHeight(double height){};
+        void setWeight(double weight) {};
+        void setShoeSize(double shoeSize){};
+        void setHairColor(std::string hairColor);
+
+        friend std::ostream& operator<<(std::ostream& out, Bigfoot bf) {};
+        friend std::istream& operator>>(std::istream& in, Bigfoot bf){};
+        friend bool operator>=(const Bigfoot& lhs, const Bigfoot& rhs){};
+        
+        
 };
