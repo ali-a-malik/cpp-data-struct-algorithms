61764a02816aa994b9e9fa5e052b7bfdfc0b5ea6
Ali A Malik <ali.ahmed.malik.07@gmail.com>
2026-02-13 22:00:25 -0800

    B: header file complete

--- homework2/NumberSequence.h
+++ homework2/NumberSequence.h
@@ -1,12 +1,7 @@
 #pragma once
-
-#include <iostream>
-#include <fstream>
-#include <cmath>
-
 class NumberSequence {
     private:
-        double length;
+        int length;
         double last;
         double sum;
         double average;
@@ -14,31 +9,27 @@ class NumberSequence {
         double max;
 
     public:
+        NumberSequence();
         void next_value(double nv);
-        // length increases at first call using setter
-        // last is assigned to nv using setter
-        // update sum is called and passes in nv method will have sum+=passed in val
-        // update average will equal sum/length
+        // length increases at first call 
+        // last is assigned to nv 
+        // sum is updated
+        // update average is called (don't need but cleaner)
         // average will call get_sum and divide that by get_length
-        // min value eval
-        // max value eval
+        // min value eval function
+        // max value eval function
 
-        void set_length(double length);
-        void set_last(double last);
-        void update_sum(double& nvRef);
+        int get_length() const {return length;}
+        double get_last() const {return last;}
+        double get_sum() const {return sum;}
+        double get_average() const {return average;}
+        double get_min() const {return min;}
+        double get_max() const {return max;}
 
-        double get_length(){return length;}
-        double get_last(){return last;}
-        double get_sum(){return sum;}
-        double get_average(){return average;}
-        double get_min(){return min;}
-        double get_max(){return max;}
-
-       
-
-
-
-
+        void update_average();
+        void update_min(double next_val);
+        void update_max(double next_val);
+        NumberSequence operator+(const NumberSequence& numseq) const;
 
 
 };
