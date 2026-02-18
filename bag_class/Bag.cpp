330f0c01047dc11f821269f3d7c5dce77beee339
Ali A Malik <ali.ahmed.malik.07@gmail.com>
2026-02-17 23:38:44 -0800

    D: Bag.cpp fixed up

--- bag_class/Bag.cpp
+++ bag_class/Bag.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <cstddef>
 #include <cassert>
+#include <algorithm>
 using namespace std;
 
 void Bag::insert(const value_type& entry){
-    assert(used<CAPACITY);
+    assert(used<=CAPACITY);
     data[used]=entry;
     used++;
 }
@@ -13,7 +14,7 @@ void Bag::insert(const value_type& entry){
 Bag::size_type Bag::count(const value_type& target) const {
     size_type count = 0;
     for(size_type i = 0; i<used; i++) {
-        if(i==target) count++;
+        if(data[i]==target) count++;
     }
 
     return count;
@@ -23,9 +24,8 @@ Bag::size_type Bag::count(const value_type& target) const {
 bool Bag::erase_one(const value_type& target) {
     for(size_type i=0; i<used; i++){
         if(data[i] == target){
-            data[i] = data[used];
+            data[i] = data[used-1];
             used--;
-            break;
             return true;
         }
     }
@@ -51,20 +51,20 @@ Bag::size_type Bag::size() const {
 }
 
 void Bag::operator +=(const Bag& addend) {
-    assert((used + addend.used) < CAPACITY);
+    assert((used + addend.used) <= CAPACITY);
     copy(addend.data, addend.data+addend.used, data+used);
-    used+=used;
+    used+=addend.used;
 }
 
 Bag operator+(const Bag& b1, const Bag& b2) {
-    assert((b1.used + b2.used) < Bag::CAPACITY);
+    assert((b1.used + b2.used) <= Bag::CAPACITY);
     Bag newBag;
     newBag+=b1;
     newBag+=b2;
     return newBag;
 }
 
-ostream& operator<<(std::ostream& out, const Bag& b) {
+ostream& operator<<(ostream& out, const Bag& b) {
     out << "{ ";
 
     for (Bag::size_type i = 0; i < b.used; i++) {
