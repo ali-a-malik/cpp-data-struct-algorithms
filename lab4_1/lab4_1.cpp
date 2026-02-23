dd64da02f256f40da52e25dcea97308c55705d24
Ali A Malik <ali.ahmed.malik.07@gmail.com>
2026-02-22 21:47:26 -0800

    D: program draft completed

--- lab4_1/lab4_1.cpp
+++ lab4_1/lab4_1.cpp
@@ -39,6 +39,35 @@ int main(){
     }
     cout << endl << endl;
 
+    // Exercise 3: Copy arr1[5] - arr1[9] into arr4
+    int arr4[5];
+
+    copy(arr1 + 5, arr1 + 10, arr4);
+
+    cout << "arr4: ";
+    for(int i = 0; i < 5; i++) {
+        cout << arr4[i] << " ";
+    }
+    cout << endl << endl;
+
+
+    // Exercise 4: Swap halves into arr5
+    int arr5[20];
+
+    // Copy first half of arr1 into second half of arr5
+    copy(arr1, arr1 + 10, arr5 + 10);
+
+    // Copy second half of arr1 into first half of arr5
+    copy(arr1 + 10, arr1 + 20, arr5);
+
+    cout << "arr5: ";
+    for(int i = 0; i < 20; i++) {
+        cout << arr5[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+
 }
 
 
