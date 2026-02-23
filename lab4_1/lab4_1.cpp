955cf92fc09e295ea0df9d52de95e292456783b9
Ali A Malik <ali.ahmed.malik.07@gmail.com>
2026-02-22 21:44:13 -0800

    B: copy to array 2

--- lab4_1/lab4_1.cpp
+++ lab4_1/lab4_1.cpp
@@ -4,9 +4,7 @@
 using namespace std;
 
 int main(){
-    // -------------------
     // Create arr1
-    // -------------------
     int arr1[20];
 
     for(int i = 0; i < 20; i++) {
@@ -18,4 +16,17 @@ int main(){
         cout << arr1[i] << " ";
     }
     cout << endl << endl;
+
+    // Exercise 1: Copy all to arr2
+    int arr2[20];
+
+    copy(arr1, arr1 + 20, arr2);
+
+    cout << "arr2: ";
+    for(int i = 0; i < 20; i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl << endl;
 }
+
+
