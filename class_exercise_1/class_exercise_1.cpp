2939e489cc3d9d0bb2b8a024ed6a956101bd34b5
Ali A Malik <ali.ahmed.malik.07@gmail.com>
2026-02-22 20:33:39 -0800

    A: initial commit

---
+++ class_exercise_1/class_exercise_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x; int y; int z;
+    int* ptr_x; int* ptr_y; int* ptr_z;
+    ptr_x = &x;
+    ptr_y =&y;
+    ptr_z = &z;
+    cin>>*ptr_x;
+    cin>>*ptr_y;
+    
+
+}
