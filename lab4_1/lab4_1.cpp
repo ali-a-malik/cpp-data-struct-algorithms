d75453817b926396757aa4844a81d2dec3318eef
Ali A Malik <ali.ahmed.malik.07@gmail.com>
2026-02-22 21:46:32 -0800

    C: Copy into arr 3

--- lab4_1/lab4_1.cpp
+++ lab4_1/lab4_1.cpp
@@ -27,6 +27,18 @@ int main(){
         cout << arr2[i] << " ";
     }
     cout << endl << endl;
+
+    // Exercise 2: Copy arr1[0] - arr1[9] into arr3
+    int arr3[10];
+
+    copy(arr1, arr1 + 10, arr3);
+
+    cout << "arr3: ";
+    for(int i = 0; i < 10; i++) {
+        cout << arr3[i] << " ";
+    }
+    cout << endl << endl;
+
 }
 
 
